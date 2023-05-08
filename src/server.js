2b2a7cd89530c22135aaeb2af99dfb8c99cacac6
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-05-08 13:56:10 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -4,9 +4,11 @@ const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+const UsersService = require('./services/postgres/UsersService');
+const users = require('./api/users');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
-  const notesService = new NotesService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,13 +19,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: new NotesService(),
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: new UsersService(),
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
